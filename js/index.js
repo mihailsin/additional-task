d969e27955f261c10a16abadd5924aa0d1991398
Mihail Sinkov <mihailsin88@gmail.com>
2021-10-18 22:24:33 +0300

    spent&earned history elements can be dynamically created

--- js/index.js
+++ js/index.js
@@ -6,24 +6,26 @@ const operationNameInput = document.querySelector('.js-operation-name');
 operationNameInput.setAttribute('name', 'operation');
 const operationAmountInput = document.querySelector('.js-operation-amount');
 operationAmountInput.setAttribute('name', 'amount');
-const operationsHisoryList = document.querySelector('.js-history-list');
-const positiveOperationsHistory =
-	operationsHisoryList.firstElementChild.firstElementChild;
-const negativeOperationsHistory =
-	operationsHisoryList.lastElementChild.firstElementChild;
+const operationHistoryList = document.querySelector('.js-history-list');
 const operationsArray = [];
-console.dir(formRef);
-
 const onSubmit = e => {
 	event.preventDefault();
 	const {
 		elements: { operation, amount },
 	} = e.currentTarget;
+	const spent = `<li class="list-group-item d-flex align-items-center text-capitalize border border-1 border-danger rounded bg-danger px-4 py-2 text-dark bg-opacity-10 mb-3">
+<span class="fs-5 history__money user-select-all">${operation.value} на сумму ${amount.value}; &#8372;</span>
+<button type="button" class="btn-close ms-auto history_delete" aria-label="Close"></button>
+</li>`;
+	const earned = `<li class="	list-group-item d-flex align-items-center text-capitalize border border-1 border-success rounded bg-success px-4 py-2 text-dark bg-opacity-10 mb-3">
+<span class="fs-5 history__money user-select-all">${operation.value} на сумму ${amount.value}; &#8372;</span>
+<button type="button" class="btn-close ms-auto history_delete" aria-label="Close"></button>
+</li>`;
 
 	if (Number(amount.value) < 0) {
-		negativeOperationsHistory.textContent = `${operation.value} на сумму ${amount.value}`;
+		operationHistoryList.insertAdjacentHTML('beforeend', spent);
 	} else if (Number(amount.value) > 0) {
-		positiveOperationsHistory.textContent = `${operation.value} на сумму ${amount.value}`;
+		operationHistoryList.insertAdjacentHTML('beforeend', earned);
 	}
 	operationsArray.push(Number(amount.value));
 	console.log(operationsArray);
